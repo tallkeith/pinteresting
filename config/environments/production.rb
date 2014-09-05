689151ec236f1917756dd2dfa2f2ec02b7843f72
tallkeith <tallestkeith@gmail.com>
2014-09-05 12:30:32 -0400

    updated aws-sdk gem, trying to fix paperclip problem with production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_KEY']
     }
   }
 end
